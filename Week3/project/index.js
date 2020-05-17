33123e668fdef2340099ab14351bebdf39d88d86
Burak Sabah <30929892+dhemir17@users.noreply.github.com>
2020-05-17 18:43:41 +0200

    commented lines are removed

--- Week3/project/index.js
+++ Week3/project/index.js
@@ -1,27 +1,17 @@
 // Your code goes in here
-
-//document.querySelector("#app").innerText = "Tip Calculator";
 const billMoney = document.getElementById('bill-money');
 const tipSelect = document.getElementById('tip-select');
 const billSharing = document.getElementById('bill-sharing');
 const calcButton = document.getElementById('calculate-btn');
 const resultArea = document.getElementById('result');
 
-//document.getElementById('result-part').style.display = "none";
-
-
-
-
 calcButton.addEventListener('click',function (){
-   // document.getElementById('result-part').style.display = "initial";
    if (billSharing.value === "" || billMoney.value === "")  {
        window.alert('You need to fill in all the fields!');
    }
    if (billSharing.value === "1") {
         document.getElementById('each').innerText = "";   }
-   
    let perc = 0;
-    
     switch ( tipSelect.value) {
         case "30":
             perc = 30;
@@ -38,11 +28,6 @@ calcButton.addEventListener('click',function (){
         case "5":
             perc = 5;
     }
-
     let result = Number(billMoney.value) * perc / 100 / Number(billSharing.value);
-
     resultArea.innerText = "$" + result.toFixed(2);
 });
-
-
-
